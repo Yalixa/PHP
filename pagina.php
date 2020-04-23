959b87cf83c882e930ad189d3afd035b42ce5bb0
Yalixa Viquez <62159545+Yalixa@users.noreply.github.com>
2020-04-23 17:55:01 -0600

    Add files via upload

--- pagina.php
+++ pagina.php
@@ -1,3 +1,12 @@
+<?php
+    if(isset($_GET["created"])){
+        echo "El post ha sido creado con exito";
+    }
+
+
+require 'database.php';
+
+?>
 <!DOCTYPE html>
 <html lang="zxx">
 
@@ -22,6 +31,7 @@
     <link rel="stylesheet" href="css/magnific-popup.css" type="text/css">
     <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
     <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link rel="stylesheet" href="css/nuevo.css" type="text/css">
 </head>
 
 <body>
@@ -29,7 +39,7 @@
     <div id="preloder">
         <div class="loader"></div>
     </div>
-    
+
     <!-- Search model -->
 	<div class="search-model">
 		<div class="h-100 d-flex align-items-center justify-content-center">
@@ -57,21 +67,11 @@
                     </a>
                 </div>
                 <div class="user-access">
-                    <a href="#">Registrarse</a>
+                    <a href="./registrarse.php">Registrarse</a>
                 </div>
                 <nav class="main-menu mobile-menu">
                     <ul>
-                        <li><a class="active" href="./pagina.php">Inicio</a></li>
-                        <li><a href="./categories.php">Comprar</a>
-                            <ul class="sub-menu">
-                                <li><a href="product-page.php">Productos</a></li>
-                                <li><a href="shopping-cart.php">Tarjeta de compras</a></li>
-                                <li><a href="check-out.php">Revisar</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="./product-page.php">Acerca de</a></li>
-                        <li><a href="./check-out.php">Blog</a></li>
-                        <li><a href="./contact.php">Contacto</a></li>
+                        <li><a class="active" href="./pagina.php">Inicio</a></li>  
                     </ul>
                 </nav>
             </div>
@@ -90,21 +90,20 @@
                 <div class="col-md-4 text-left text-lg-center">
                     <div class="header-item">
                         <img src="img/icons/voucher.png" alt="">
-                        <p>Promociones.</p>
+                        <p>TIPS.</p>
                     </div>
                 </div>
                 <div class="col-md-4 text-left text-xl-right">
                     <div class="header-item">
                     <img src="img/icons/sales.png" alt="">
-                    <p>Desc en ropa seleccionada. </p>
+                    <p>Desc en la tienda fisica. </p>
                 </div>
                 </div>
             </div>
         </div>
     </div>
-    <!-- Header Info End -->
-    <!-- Header End -->
 
+    
     <!-- Hero Slider Begin -->
     <section class="hero-slider">
         <div class="hero-items owl-carousel">
@@ -114,7 +113,6 @@
                         <div class="col-lg-12">
                             <h1>2020</h1>
                             <h2>Lookbook.</h2>
-                            <a href="#" class="primary-btn">Ver más</a>
                         </div>
                     </div>
                 </div>
@@ -125,7 +123,6 @@
                         <div class="col-lg-12">
                             <h1>2020</h1>
                             <h2>Lookbook.</h2>
-                            <a href="#" class="primary-btn">Ver más</a>
                         </div>
                     </div>
                 </div>
@@ -136,271 +133,26 @@
                         <div class="col-lg-12">
                             <h1>2020</h1>
                             <h2>Lookbook.</h2>
-                            <a href="#" class="primary-btn">Ver más</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </section>
-    <!-- Hero Slider End -->
 
-    <!-- Features Section Begin -->
-    <section class="features-section spad">
-        <!-- Features Box -->
-        <div class="features-box">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-6">
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <div class="single-box-item first-box">
-                                    <img src="img/f-box-1.jpg" alt="">
-                                    <div class="box-text">
-                                        <span class="trend-year">Fiestas 2020</span>
-                                        <h2>Joyería</h2>
-                                        <span class="trend-alert">Alarma de tendencia</span>
-                                        <a href="#" class="primary-btn">Ver más</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-lg-12">
-                                <div class="single-box-item second-box">
-                                    <img src="img/f-box-2.jpg" alt="">
-                                    <div class="box-text">
-                                        <span class="trend-year">Tendencias 2020</span>
-                                        <h2>Calzado</h2>
-                                        <span class="trend-alert">Bold & Black</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6">
-                        <div class="single-box-item large-box">
-                            <img src="img/f-box-3.jpg" alt="">
-                            <div class="box-text">
-                                <span class="trend-year">Fiestas 2020</span>
-                                <h2>Colección</h2>
-                                <div class="trend-alert">En tendencia</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- Features Section End -->
-
-    <!-- Latest Product Begin -->
-    <section class="latest-products spad">
-        <div class="container">
-            <div class="product-filter">
-                <div class="row">
-                    <div class="col-lg-12 text-center">
-                        <div class="section-title">
-                            <h2>Últimos Productos</h2>
-                        </div>
-                        <ul class="product-controls">
-                            <li data-filter="*">Todo</li>
-                            <li data-filter=".dresses">Vestidos</li>
-                            <li data-filter=".bags">Bolsos</li>
-                            <li data-filter=".shoes">Zapatos</li>
-                            <li data-filter=".accesories">Accesorios</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="row" id="product-list">
-                <div class="col-lg-3 col-sm-6 mix all dresses bags">
-                    <div class="single-product-item">
-                        <figure>
-                            <a href="#"><img src="img/products/img-1.jpg" alt=""></a>
-                            <div class="p-status">Nuevo</div>
-                        </figure>
-                        <div class="product-text">
-                            <h6>Vestido de baño</h6>
-                            <p>₡20.000</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-sm-6 mix all dresses bags">
-                    <div class="single-product-item">
-                        <figure>
-                            <a href="#"><img src="img/products/img-2.jpg" alt=""></a>
-                            <div class="p-status sale">Rebaja</div>
-                        </figure>
-                        <div class="product-text">
-                            <h6>Vestido largo amarillo (Maxi Dress)</h6>
-                            <p>₡30.000</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-sm-6 mix all shoes accesories">
-                    <div class="single-product-item">
-                        <figure>
-                            <a href="#"><img src="img/products/img-3.jpg" alt=""></a>
-                            <div class="p-status">Nuevo</div>
-                        </figure>
-                        <div class="product-text">
-                            <h6>Body de una pieza</h6>
-                            <p>₡13.500</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-sm-6 mix all shoes accesories">
-                    <div class="single-product-item">
-                        <figure>
-                            <a href="#"><img src="img/products/img-4.jpg" alt=""></a>
-                            <div class="p-status popular">Popular</div>
-                        </figure>
-                        <div class="product-text">
-                            <h6>Vestido Azul con detalles</h6>
-                            <p>₡35.500</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-sm-6 mix all dresses shoes">
-                    <div class="single-product-item">
-                        <figure>
-                            <a href="#"><img src="img/products/img-5.jpg" alt=""></a>
-                            <div class="p-status">new</div>
-                        </figure>
-                        <div class="product-text">
-                            <h6>Vestido verde con detalles</h6>
-                            <p>$22.90</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-sm-6 mix all accesories bags">
-                    <div class="single-product-item">
-                        <figure>
-                            <a href="#"><img src="img/products/img-6.jpg" alt=""></a>
-                            <div class="p-status sale">sale</div>
-                        </figure>
-                        <div class="product-text">
-                            <h6>Vestido Amarillo</h6>
-                            <p>$25.90</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-sm-6 mix all dresses bags">
-                    <div class="single-product-item">
-                        <figure>
-                            <a href="#"><img src="img/products/img-7.jpg" alt=""></a>
-                        </figure>
-                        <div class="product-text">
-                            <h6>Body</h6>
-                            <p>$19.90</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-sm-6 mix all accesories bags">
-                    <div class="single-product-item">
-                        <figure>
-                            <a href="#"><img src="img/products/img-8.jpg" alt=""></a>
-                            <div class="p-status popular">Popular</div>
-                        </figure>
-                        <div class="product-text">
-                            <h6>Vestido Azul</h6>
-                            <p>$35.50</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- Latest Product End -->
-
-    <!-- Lookbok Section Begin -->
-    <section class="lookbok-section">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-4 offset-lg-1">
-                    <div class="lookbok-left">
-                        <div class="section-title">
-                            <h2>2020 <br />#Lookbook</h2>
-                        </div>
-                        <p>Los nombres más importantes de la moda han reescrito su definición de glamour moderno para la Primavera-Verano 2020
-                            para incluir un toque hecho a mano. Tan poco pretencioso como puede ser el crochet de lujo, ciertamente no está hecho para la playa.
-                            En cambio, deja que estos tejidos abiertos inspiren tu atuendo de lujo para la oficina o para un coctel al llegar la primavera.</p>
-                        <a href="#" class="primary-btn look-btn">Ver más</a>
-                    </div>
-                </div>
-                <div class="col-lg-5 offset-lg-1">
-                    <div class="lookbok-pic">
-                        <img src="img/lookbok.jpg" alt="">
-                        <div class="pic-text">Fashion</div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- Lookbok Section End -->
-
-    <!-- Logo Section Begin -->
-    <div class="logo-section spad">
-        <div class="logo-items owl-carousel">
-            <div class="logo-item">
-                <img src="img/logos/logo-1.png" alt="">
-            </div>
-            <div class="logo-item">
-                <img src="img/logos/logo-2.png" alt="">
-            </div>
-            <div class="logo-item">
-                <img src="img/logos/logo-3.png" alt="">
-            </div>
-            <div class="logo-item">
-                <img src="img/logos/logo-4.png" alt="">
-            </div>
-            <div class="logo-item">
-                <img src="img/logos/logo-5.png" alt="">
-            </div>
-        </div>
-    </div>
-    <!-- Logo Section End -->
-
-    <!-- Footer Section Begin -->
-    <footer class="footer-section spad">
-        <div class="container">
-            <div class="newslatter-form">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <form action="#">
-                            <input type="text" placeholder="Su dirección de correo electrónico">
-                            <button type="submit">Suscribirse a nuestro boletín de noticias</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <!-- <div class="footer-widget">
-                <div class="row">
-            
-                </div>
-            </div> -->
-        </div>
-        <div class="social-links-warp">
-			<div class="container">
-				<div class="social-links">
-					<a href="" class="instagram"><i class="fa fa-instagram"></i><span>instagram</span></a>
-					<a href="" class="pinterest"><i class="fa fa-pinterest"></i><span>pinterest</span></a>
-					<a href="" class="facebook"><i class="fa fa-facebook"></i><span>facebook</span></a>
-					<a href="" class="twitter"><i class="fa fa-twitter"></i><span>twitter</span></a>
-					<a href="" class="youtube"><i class="fa fa-youtube"></i><span>youtube</span></a>
-					<a href="" class="tumblr"><i class="fa fa-tumblr-square"></i><span>tumblr</span></a>
-				</div>
-			</div>
-
-<div class="container text-center pt-5">
-                <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Clothes Shop by Yalixa Viquez </a>
-  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-            </div>
+    <section a>
+                                    <?php
+                                    include("mostrar_post.php");
+                                    ?>     
+    </section a>
 
 
-		</div>
+
+    <footer>
+    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Clothes Shop by Yalixa Viquez </a>
+
     </footer>
-    <!-- Footer Section End -->
+
 
     <!-- Js Plugins -->
     <script src="js/jquery-3.3.1.min.js"></script>
